6b7302557c69fae40c3204d26b22f650e2353f05
Tyler Thomann <Tyler Thomann>
2016-03-08 15:52:44 -0600

    working dice roll function

--- MainDice.java
+++ MainDice.java
@@ -1,10 +1,10 @@
-
+package DiceGameJava;
 public class MainDice {
 
 	public static void main(String[] args) {
 		
-		DiceRoll dice1 = new DiceRoll();
-		System.out.println(dice1.getDie1());
+		ChooseDice instanceDice = new ChooseDice();
+		instanceDice.DiceSelect();
 
 	}
 
--- Player.java
+++ Player.java
@@ -1,4 +1,22 @@
-
-public class Player {
+package DiceGameJava;
+import java.util.Scanner;
+public class Player{
+	
+	Scanner input = new Scanner(System.in);
+	
+	public Player(){
+		String name = input.next();
+	}
+	
+	public void makePlayers(){
+		Player player = new Player();
+	}
+	
+	public void playerDiceChoice(){
+		
+	}
+	
 
 }
+//player choice
+//player name
